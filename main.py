952b97a3f7cc078c09ff5ab9e323b5075ff9d338
Jim Flores <jimflores5@gmail.com>
2021-03-25 10:44:46 -0500

    Removed os import.

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from flask import Flask, request, redirect, render_template, session
-import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.secret_key = os.urandom(32)
+app.secret_key = 'Chemistry'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
