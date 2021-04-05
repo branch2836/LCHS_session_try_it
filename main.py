6d91f00948a7823c72e877a5f0c57785776cf16d
Jim Flores <jflores@launchcode.org>
2021-04-05 16:20:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, redirect, render_template, session
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.secret_key = 'This_is_NOT_a_good_secret_key'
+app.secret_key = 'K>~EEAnH_x,Z{q.43;NmyQiNz1^Yr7'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
