36457e4d6c770e1ca8da8c79f6b4dad6e795222d
Branch, Christopher <Christopher_Branch@comcast.com>
2023-01-05 16:09:42 -0500

    end of 23.6

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ app.secret_key = 'K>~EEAnH_x,Z{q.43;NmyQiNz1^Yr7'
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        pass
+        new_item = request.form["new_item"]
+        current_list = session['list_name']
+        if new_item not in current_list:
+            current_list.append(new_item)
+        else:
+            current_list.remove(new_item)
+        session['list_name'] = current_list
+
     else:
-        session['list_name'] =['huey', 'duey', 'louie']
+        if 'list_name' not in session:
+            session['list-name'] =[]
     return render_template('index.html')
 
 if __name__ == '__main__':
